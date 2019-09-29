0981db7c7b9e22ab224a00cacf751e42ef7b5b5d
cohessel <larissahessel@outlook.com>
2019-09-29 14:20:10 -0300

    o inicio do inicio

---
+++ projetinho-oshi/src/main/java/com/mycompany/projetinho/oshi/cpu.java
@@ -0,0 +1,6 @@
+package com.mycompany.projetinho.oshi;
+
+public class cpu {
+    private Integer cpuPorcentagem = 0;
+    private Double cpuGhz = 0.0;
+}
---
+++ projetinho-oshi/src/main/java/com/mycompany/projetinho/oshi/memoria.java
@@ -0,0 +1,6 @@
+package com.mycompany.projetinho.oshi;
+
+public class memoria {
+    private Integer memoriaPorcentagem = 0;
+    private Double memoriaGb = 0.0;
+}
